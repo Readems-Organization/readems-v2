fe97b5f2aa2c833deabd157293de0282b3fe0a6b
Timothy Attah <timothyattah733@gmail.com>
2022-09-01 04:36:30 +0100

    Changed and added web url

--- frontend/src/redux/api/index.js
+++ frontend/src/redux/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const baseURL = process.env.REACT_APP_BACKEND_API;
+export const baseURL =
+  process.env.REACT_APP_BACKEND_API || 'http://localhost:5000';
 
 const API = axios.create({ baseURL: baseURL });
 
