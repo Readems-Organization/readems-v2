f3d323f48aed4a6f43d2e2c7ba442e8e92ed7f17
Timothy Attah <timothyattah733@gmail.com>
2022-09-04 20:23:13 +0100

    Setup vercel -remove

--- frontend/src/Translate.jsx
+++ frontend/src/Translate.jsx
@@ -4,3 +4,19 @@
 // export const Translate = ReactDOM.render(function loadGoogleTranslate() {
 //   new google.translate.TranslateElement('google_element');
 // }, document.getElementById('google_element'));
+
+// {
+//   "version": 2,
+//   "builds": [
+//     {
+//       "src": "./backend/server.js",
+//       "use": "@vercel/node"
+//     }
+//   ],
+//   "routes": [
+//     {
+//       "src": "/(.*)",
+//       "dest": "/"
+//     }
+//   ]
+// }
